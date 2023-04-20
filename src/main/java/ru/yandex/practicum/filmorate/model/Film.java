3b9764243ac1a32eb70efb88f2d8dd1baa5c1f15
aleckid77@gmail.com <MyGitHub1>
2023-04-20 21:17:28 +0300

    feat: tests

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -3,12 +3,14 @@ package ru.yandex.practicum.filmorate.model;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import ru.yandex.practicum.filmorate.annotations.DateReleaseIsValid;
 
 import java.time.LocalDate;
 
 @Data
+@AllArgsConstructor
 public class Film {
 
     private Integer id;
