adaf66b57c8a06bdac0106d20e5aba506295dfad
aleckid77@gmail.com <MyGitHub1>
2023-05-29 15:44:00 +0300

    fix: user

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -8,7 +8,6 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
 import java.time.LocalDate;
-import java.util.HashSet;
 import java.util.Set;
 
 @Data
@@ -28,7 +27,7 @@ public class User {
     private String name;
 
     @Past
-    private LocalDate birthday;
+    private LocalDate birthdate;
 
-    private final Set<Long> friends = new HashSet<>();
+    private Set<Long> friends;
 }
