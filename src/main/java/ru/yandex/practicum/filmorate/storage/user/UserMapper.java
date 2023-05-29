649ad9f432d47acaa1ac30e9cc2ee951f16a5d59
aleckid77@gmail.com <MyGitHub1>
2023-05-29 15:46:33 +0300

    feat: user and film mappers

---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserMapper.java
@@ -0,0 +1,31 @@
+package ru.yandex.practicum.filmorate.storage.user;
+
+import org.springframework.jdbc.core.RowMapper;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class UserMapper implements RowMapper<User> {
+    @Override
+    public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+        return User.builder()
+                .id(rs.getLong("id"))
+                .email(rs.getString("email"))
+                .name(rs.getString("name"))
+                .birthdate((rs.getDate("birthdate")).toLocalDate())
+                .friends(parseToSet(rs.getString("friends")))
+                .build();
+    }
+
+    private Set<Long> parseToSet(String friends) {
+        return friends.isBlank() && friends.isEmpty() ? Collections.emptySet() :
+                Arrays.stream(friends.split(","))
+                        .map(Long::parseLong)
+                        .collect(Collectors.toSet());
+    }
+}
