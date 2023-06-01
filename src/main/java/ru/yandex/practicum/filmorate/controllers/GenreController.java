b1906cb249aecb5dba2f12dcc545e7fd65aff3bd
aleckid77@gmail.com <MyGitHub1>
2023-06-01 11:09:11 +0300

    feat: GenreController feat: MpaRatingController

---
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/GenreController.java
@@ -0,0 +1,31 @@
+package ru.yandex.practicum.filmorate.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.yandex.practicum.filmorate.model.Genre;
+import ru.yandex.practicum.filmorate.service.GenreService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/genres")
+public class GenreController {
+
+    private final GenreService genreService;
+
+    public GenreController(GenreService genreService) {
+        this.genreService = genreService;
+    }
+
+    @GetMapping
+    public List<Genre> getAll() {
+        return genreService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Genre get(@PathVariable long id) {
+        return genreService.get(id);
+    }
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/MpaRatingController.java
@@ -0,0 +1,31 @@
+package ru.yandex.practicum.filmorate.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.yandex.practicum.filmorate.model.MpaRating;
+import ru.yandex.practicum.filmorate.service.MpaRatingService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/mpa")
+public class MpaRatingController {
+
+    private final MpaRatingService mpaRatingService;
+
+    public MpaRatingController(MpaRatingService mpaRatingService) {
+        this.mpaRatingService = mpaRatingService;
+    }
+
+    @GetMapping
+    public List<MpaRating> getAll() {
+        return mpaRatingService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public MpaRating get(@PathVariable long id) {
+        return mpaRatingService.get(id);
+    }
+}
