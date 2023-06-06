f8eb09834965ea4ca845a718a0330d9cb057cb27
aleckid77@gmail.com <MyGitHub1>
2023-06-06 18:49:29 +0300

    refactor: FilmService

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -4,13 +4,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.exception.AlreadyExistException;
-import ru.yandex.practicum.filmorate.exception.NotExistException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
 
 import java.util.Comparator;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -41,15 +39,6 @@ public class FilmService {
     public Film updateFilm(Film film) {
         get(film.getId());
         return filmStorage.update(film);
-//        return filmStorage.findAll().stream()
-//                .filter(f -> f.getId().equals(film.getId()))
-//                .findFirst()
-//                .map(f -> {
-//                    filmStorage.update(film);
-//                    log.info("updated film with id: {} and name: {}", film.getId(), film.getName());
-//                    return film;
-//                })
-//                .orElseThrow(() -> new ValidationException("validation failed for film name: " + film.getName()));
     }
 
     public void like(long filmId, long userId) {
@@ -78,9 +67,7 @@ public class FilmService {
                 .collect(Collectors.toList());
     }
 
-    //TODO
     public Film get(long id) {
-        return Optional.ofNullable(filmStorage.get(id))
-                .orElseThrow(() -> new NotExistException("Film with id=" + id + " not exist"));
+        return filmStorage.get(id);
     }
 }
