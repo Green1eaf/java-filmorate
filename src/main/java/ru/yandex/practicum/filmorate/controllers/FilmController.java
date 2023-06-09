f982d5c3fbc41e6094a5ddb3c68777d2031e93db
aleckid77@gmail.com <MyGitHub1>
2023-06-09 20:20:39 +0300

    fix

--- src/main/java/ru/yandex/practicum/filmorate/controllers/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/FilmController.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate.controllers;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.service.FilmService;
-import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
 
 import javax.validation.Valid;
 import java.util.List;
