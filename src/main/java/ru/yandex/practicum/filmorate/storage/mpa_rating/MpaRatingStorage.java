5dcf16dd81b1545b2fb673dae51cbda022becb65
aleckid77@gmail.com <MyGitHub1>
2023-06-26 12:46:25 +0300

    refactor: MpaRatingService and MpaRatingDbStorage

--- src/main/java/ru/yandex/practicum/filmorate/storage/mpa_rating/MpaRatingStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/mpa_rating/MpaRatingStorage.java
@@ -3,10 +3,11 @@ package ru.yandex.practicum.filmorate.storage.mpa_rating;
 import ru.yandex.practicum.filmorate.model.Mpa;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface MpaRatingStorage {
 
-    Mpa get(long id);
+    Optional<Mpa> get(long id);
 
     List<Mpa> getAll();
 }
