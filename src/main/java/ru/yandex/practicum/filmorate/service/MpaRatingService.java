f1ee68756a76e7d5d91b2c2f4a3c69ecc226f327
aleckid77@gmail.com <MyGitHub1>
2023-06-26 12:37:26 +0300

    refactor: MpaRatingService and MpaRatingDbStorage

--- src/main/java/ru/yandex/practicum/filmorate/service/MpaRatingService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/MpaRatingService.java
@@ -1,12 +1,15 @@
 package ru.yandex.practicum.filmorate.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.exception.NotExistException;
 import ru.yandex.practicum.filmorate.model.Mpa;
 import ru.yandex.practicum.filmorate.storage.mpa_rating.MpaRatingStorage;
 
 import java.util.List;
 
 @Service
+@Slf4j
 public class MpaRatingService {
 
     private final MpaRatingStorage mpaRatingStorage;
@@ -16,10 +19,13 @@ public class MpaRatingService {
     }
 
     public Mpa get(long id) {
-        return mpaRatingStorage.get(id);
+        log.info("Get MpaRating with id=" + id);
+        return mpaRatingStorage.get(id)
+                .orElseThrow(() -> new NotExistException("Rating with id=" + id + " not exists"));
     }
 
     public List<Mpa> getAll() {
+        log.info("Get all MpaRatings");
         return mpaRatingStorage.getAll();
     }
 }
